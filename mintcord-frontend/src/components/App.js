825081700df5137f9cee7deb1bffcb9a3cf8e0ea
alex.hanwoolkim <alex.hanwoolkim@gmail.com>
2020-01-16 15:21:52 +0900

    home route & LoginForm, Button

--- mintcord-frontend/src/components/App.js
+++ mintcord-frontend/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import Template from 'components/Template';
+import Home from 'pages/Home';
 
 const App = () => {
   return (
-	  <Template/>
+    <>
+      <Route exact path="/" component={Home} />
+      <Route path="/app" component={Template} />
+    </>
   )
 }
 
---
+++ mintcord-frontend/src/components/common/Button/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Button.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const Div = ({children, ...rest}) => <div {...rest}>{children}</div>
+
+const Button = ({
+  children, to, onClick, disabled, theme = 'default',
+}) => {
+
+  const Element = (to && !disabled) ? Link : Div;
+
+  return (
+    <Element 
+      to={to} 
+      className={cx('button', theme, { disabled })} 
+      onClick={disabled ? () => null : onClick}>
+      {children}
+    </Element>
+  )
+}
+  
+export default Button;
